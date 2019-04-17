d7381b42ddb495a4671f17cdb72e4ad2994f6e43
Javier Gomez <javier.gomez.p@ug.uchile.cl>
2019-04-17 19:07:49 +0000

    implement helloworld

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def hello_world(request):
+    return HttpResponse("<h1>hello world</h1>")
